cd5d937a8d34d5984e4e9c826e097fccfd015576
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-07-27 14:16:36 +0100

    fix call to build script

--- ci/build-target.sh
+++ ci/build-target.sh
@@ -7,5 +7,8 @@ source "${SCRIPT_DIRECTORY}/common.sh"
 
 ensureSet CI_PROJECT_DIR
 
+target=$1
+echo "Building arvados for ${target}"
+
 export WORKSPACE=${CI_PROJECT_DIR}/subrepos/arvados
-${CI_PROJECT_DIR}/subrepos/arvados/build/run-build-packages-one-target.sh $1
+${CI_PROJECT_DIR}/subrepos/arvados/build/run-build-packages-one-target.sh --target ${target}
