fbaf07fa3e84a3038307b3d85213a75af27356df
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-08-10 11:56:49 +0100

    WIP: debugging dockerd-entrypoint.sh

--- docker/arvados-build-debian8/dockerd-entrypoint.sh
+++ docker/arvados-build-debian8/dockerd-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-set -e
 
-nohup dockerd --host=${DOCKER_HOST} > /var/log/docker.out 2> /var/log/docker.err &
+nohup dockerd --host=${DOCKER_HOST} > /var/log/docker-dind.out 2> /var/log/docker-dind.err &
 dockerpid=$!
 
 # wait for docker.err to log 'acceptconnections() = OK'
@@ -10,11 +9,11 @@ echo -n "Waiting for docker daemon to be ready."
 while test -z "${docker_ready}"
 do
     # docker hasn't reported that it is ready yet, check if it has exited
-    grep -q exit /var/log/docker.err && echo " docker daemon has exited!" && echo "docker log:" && cat /var/log/docker.err && exit 1
+    grep -q exit /var/log/docker-dind.err && echo " docker daemon has exited!" && echo "docker log:" && cat /var/log/docker-dind.err && exit 1
 
     echo -n "."
     sleep 0.05 || sleep 1
-    docker_ready=$(grep 'acceptconnections.*=.*OK' /var/log/docker.err)
+    docker_ready=$(grep 'acceptconnections.*=.*OK' /var/log/docker-dind.err)
 done
 echo " docker ready."
 
