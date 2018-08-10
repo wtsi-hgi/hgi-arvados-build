9ebe77bac23f3fe95aaa07e3fe1665564a881ea5
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-08-10 12:06:49 +0100

    check for the new API listen on message in docker-dind.err

--- docker/arvados-build-debian8/dockerd-entrypoint.sh
+++ docker/arvados-build-debian8/dockerd-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nohup dockerd --host=${DOCKER_HOST} > /var/log/docker-dind.out 2> /var/log/docker-dind.err &
+nohup dockerd --host=${DOCKER_HOST} > /dev/null 2> /var/log/docker-dind.err &
 dockerpid=$!
 
 # wait for docker.err to log 'acceptconnections() = OK'
@@ -9,11 +9,11 @@ echo -n "Waiting for docker daemon to be ready."
 while test -z "${docker_ready}"
 do
     # docker hasn't reported that it is ready yet, check if it has exited
-    grep -q exit /var/log/docker-dind.err && echo " docker daemon has exited!" && echo "docker log:" && cat /var/log/docker-dind.err && exit 1
+    grep -q exit /var/log/docker-dind.err && echo "docker daemon has exited!" && echo "docker log:" && cat /var/log/docker-dind.err && exit 1
 
     echo -n "."
     sleep 0.05 || sleep 1
-    docker_ready=$(grep 'acceptconnections.*=.*OK' /var/log/docker-dind.err)
+    docker_ready=$(grep 'API listen on /var/run/docker-dind.sock' /var/log/docker-dind.err)
 done
 echo " docker ready."
 
